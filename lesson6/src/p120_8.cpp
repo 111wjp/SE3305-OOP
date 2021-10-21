70c034d9a2bef00fe0c3a5c752018b711ce23ae7
zpyqaq <2433208256@qq.com>
2021-10-21 14:02:17 +0800

    cd

---
+++ lesson6/src/p120_8.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+// p120 8
+class Student{
+	public:
+		Student(int n,float s):num(n),score(s){
+		}
+		void change(int n,float s){
+			num = n;
+			score = s;
+		}
+		void display(){
+			cout<<num<<" "<<score<<endl;
+		}
+	private:
+		int num;
+		float score;
+};
+
+void fun(Student &stu){
+	stu.display();
+	stu.change(101,78.5);
+	stu.display();
+}
+
+int main(){
+	Student stud(101,78.5);
+	fun(stud);
+	return 0;
+}
+
